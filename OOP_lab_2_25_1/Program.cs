028fe0393cb0d1518a581b4be45243a2cfa426c9
Роман Данилевич <dwordchanel@gmail.com>
2020-05-26 18:57:24 +0300

    Add project files.

---
+++ OOP_lab_2_25_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP_lab_2_25_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            switch (a % 10)
+            {
+                case 1:
+                    Console.WriteLine("{0} рiк.", a);
+                    break;
+
+                case 2:
+                case 3:
+                case 4:
+                    Console.WriteLine("{0} рoки.", a);
+                    break;
+
+                default:
+                    Console.WriteLine("{0} рoкiв.", a);
+                    break;
+            }
+        }
+    }
+}
